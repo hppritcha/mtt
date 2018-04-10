f13db5fd569f3cd6c990c530af0a1c8b6bb1a261
Howard Pritchard <howardp@lanl.gov>
2018-04-10 09:22:37 -0600

    update scripts for changed perl env
    Signed-off-by: Howard Pritchard <howardp@lanl.gov>

--- otono_v3.1.x_script.sh
+++ otono_v3.1.x_script.sh
@@ -6,10 +6,10 @@ cd /home/hpp/mtt
 the_date=`date`
 echo $the_date >> ${HOME}/mtt/run_start_date_v3.1.x
 
-PERL5LIB="/home/hpp/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}"; export PERL5LIB;
-PERL_LOCAL_LIB_ROOT="/home/hpp/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}"; export PERL_LOCAL_LIB_ROOT;
-PERL_MB_OPT="--install_base \"/home/hpp/perl5\""; export PERL_MB_OPT;
-PERL_MM_OPT="INSTALL_BASE=/home/hpp/perl5"; export PERL_MM_OPT;
+#PERL5LIB="/home/hpp/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}"; export PERL5LIB;
+#PERL_LOCAL_LIB_ROOT="/home/hpp/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}"; export PERL_LOCAL_LIB_ROOT;
+#PERL_MB_OPT="--install_base \"/home/hpp/perl5\""; export PERL_MB_OPT;
+#PERL_MM_OPT="INSTALL_BASE=/home/hpp/perl5"; export PERL_MM_OPT;
 
-timeout 260m client/mtt --file ompi-otono-v3.1.x.ini --mpi-get --mpi-install --test-get --test-build --test-run  --verbose --force
+timeout 260m client/mtt --file ompi-otono-v3.1.x.ini --mpi-get --mpi-install --test-get --test-build --test-run  --verbose
 
