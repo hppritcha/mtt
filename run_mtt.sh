675a14ebe696fe2d9f81ed5255daa8eaf09324e7
Howard Pritchard <howardp@lanl.gov>
2019-12-18 18:48:04 +0000

    add ini files and scripts for theta
    Signed-off-by: Howard Pritchard <howardp@lanl.gov>
    (cherry picked from commit 2a0db60ac7ee6582d579c147ef9793c4a24a0cb8)

---
+++ run_mtt.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -l
+
+module load miniconda-3/latest
+
+#
+# somethings borked with Intel at the moment
+#
+if $( echo ${LOADEDMODULES} | grep --quiet 'PrgEnv-intel' ); then
+    module swap PrgEnv-intel PrgEnv-gnu
+fi
+
+if [ $# -eq 0 ] ; then
+  BRANCH=master
+else
+  BRANCH=$1
+fi
+cd $HOME/mtt
+export MTT_HOME=$PWD
+echo "============== Testing $BRANCH  ==============="
+pyclient/pymtt.py --verbose  get_ompi_master.ini
+#qsub -n 128 -t 160 -A CSC250STPR27 ./run_imb.sh $BRANCH
+jobid=`qsub -n 8 --jobname ompi.$BRANCH -q debug-flat-quad -t 60 -A CSC250STPR27 ./run_mtt_backend.sh $BRANCH`
+export QSTAT_HEADER="State"
+nlines=`qstat $jobid | wc -l`
+while [ $nlines != 0 ]
+do
+sleep 120
+nlines=`qstat $jobid | wc -l`
+done
+pyclient/pymtt.py --verbose  iu_reporter.ini
+
