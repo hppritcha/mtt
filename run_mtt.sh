c6cfce1e4bdbd2787a3499db8e16d7586b8c58c1
Howard Pritchard <howardp@lanl.gov>
2019-12-20 17:07:15 +0000

    additional ini file, script fixes
    to support testing other branches
    
    Signed-off-by: Howard Pritchard <howardp@lanl.gov>

--- run_mtt.sh
+++ run_mtt.sh
@@ -17,7 +17,12 @@ fi
 cd $HOME/mtt
 export MTT_HOME=$PWD
 echo "============== Testing $BRANCH  ==============="
-pyclient/pymtt.py --verbose  get_ompi_master.ini
+pyclient/pymtt.py --verbose  get_ompi_$BRANCH.ini
+if [ $? -ne 0 ]
+then
+    echo "Something went wrong with fetch/build phase"
+    exit -1
+fi
 #qsub -n 128 -t 160 -A CSC250STPR27 ./run_imb.sh $BRANCH
 jobid=`qsub -n 8 --jobname ompi.$BRANCH -q debug-flat-quad -t 60 -A CSC250STPR27 ./run_mtt_backend.sh $BRANCH`
 export QSTAT_HEADER="State"
@@ -27,5 +32,5 @@ do
 sleep 120
 nlines=`qstat $jobid | wc -l`
 done
-pyclient/pymtt.py --verbose  iu_reporter.ini
+pyclient/pymtt.py --verbose  iu_reporter_$BRANCH.ini
 
