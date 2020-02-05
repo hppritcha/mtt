1cf88d1216ed4f39b076d1a816d964fe90e0c06a
Howard Pritchard <howardp@lanl.gov>
2020-02-05 11:03:32 -0800

    add cori run_mtt.sh master ini
    and a snapshopt of the crontab on cori08
    
    Signed-off-by: Howard Pritchard <howardp@lanl.gov>

--- run_mtt_backend.sh
+++ run_mtt_backend.sh
@@ -1,7 +1,12 @@
 #!/bin/bash -l
 
-module load miniconda-3/latest
-module swap PrgEnv-intel PrgEnv-gnu
+module load python/3.7-anaconda-2019.07
+#
+# somethings borked with Intel at the moment
+#
+if $( echo ${LOADEDMODULES} | grep --quiet 'PrgEnv-intel' ); then
+    module swap PrgEnv-intel PrgEnv-gnu
+fi
 if [ $# -eq 0 ] ; then
   BRANCH=master
 else
@@ -9,5 +14,5 @@ else
 fi
 cd $HOME/mtt
 export MTT_HOME=$PWD
-pyclient/pymtt.py --verbose --trial run_ibm_tests_alps.ini
+pyclient/pymtt.py --verbose run_ibm_tests_mpirun_$BRANCH.ini
 
