811460f907da6de73e676f2f97d46ebfd39fa9e7
Howard Pritchard <howardp@lanl.gov>
2020-04-16 14:13:07 -0700

    turn off verbose output from logger
    when doing checkpoint/restart
    
    Signed-off-by: Howard Pritchard <howardp@lanl.gov>

--- pylib/Utilities/Logger.py
+++ pylib/Utilities/Logger.py
@@ -221,7 +221,7 @@ class Logger(BaseMTTUtility):
             fh = open(cpfile + '.pkl', 'rb')
             self.results = pickle.load(fh)
             fh.close()
-            self.outputLog()
+#           self.outputLog()
         except IOError:
             print("Failed to open checkpoint file for reading", file=self.fh)
             sys.stdout.flush()
