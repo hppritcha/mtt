bd53fd599cf96ef3a4d76f2fbaf8726aa024639c
Howard Pritchard <howardp@lanl.gov>
2022-03-15 11:05:01 -0700

    script and ini file changes for perlmutter
    perlmutter backend nodes can access the internet
    so simplify a bunch of stuff based on that fact.
    
    Signed-off-by: Howard Pritchard <howardp@lanl.gov>

--- run_mtt.sh
+++ run_mtt.sh
@@ -1,35 +1,17 @@
 #!/bin/bash -l
 
-module load python/3.7-anaconda-2019.07
+module load python
 
-#
-# somethings borked with Intel at the moment
-#
-if $( echo ${LOADEDMODULES} | grep --quiet 'PrgEnv-intel' ); then
-    module swap PrgEnv-intel PrgEnv-gnu
-fi
-
-cd $HOME/mtt
+cd $HOME/mtt_perlmutter
 if [ $# -eq 0 ] ; then
   BRANCH=master
 else
   BRANCH=$1
 fi
 SCRATCH_FILE=$BRANCH"_scratch"
-SCRATCH_DIR=/global/homes/h/hpp/mtt/$SCRATCH_FILE
+SCRATCH_DIR=/global/homes/h/hpp/mtt_perlmutter/$SCRATCH_FILE
 rm -f -r $SCRATCH_DIR
 export MTT_HOME=$PWD
 echo "============== Testing $BRANCH  ==============="
 pyclient/pymtt.py --verbose  get_ompi_$BRANCH.ini
-if [ $? -ne 0 ]
-then
-    echo "Something went wrong with fetch/build phase"
-else
-    echo "============== Submitting batch job for Testing $BRANCH  ==============="
-    jobid=`sbatch -o slurm.$BRANCH.out --wait --parsable -N 4 -C knl --time=8:00:00 -qregular --tasks-per-node=32 -J $BRANCH ./run_mtt_backend.sh $BRANCH`
-    if [ $jobid -eq 1 ]; then
-        echo "Something went wrong with batch job"
-    fi
-fi
-pyclient/pymtt.py --verbose  iu_reporter_$BRANCH.ini
 
--- run_mtt_backend.sh
+++ run_mtt_backend.sh
@@ -1,18 +1,16 @@
 #!/bin/bash -l
 
-module load python/3.7-anaconda-2019.07
+module load python
+module load PrgEnv-gnu
 #
 # somethings borked with Intel at the moment
 #
-if $( echo ${LOADEDMODULES} | grep --quiet 'PrgEnv-intel' ); then
-    module swap PrgEnv-intel PrgEnv-gnu
-fi
 if [ $# -eq 0 ] ; then
   BRANCH=master
 else
   BRANCH=$1
 fi
-cd $HOME/mtt
+cd $HOME/mtt_branch
 export MTT_HOME=$PWD
 pyclient/pymtt.py --verbose run_ibm_tests_mpirun_$BRANCH.ini
 
