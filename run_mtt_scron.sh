f51d0253b9a80ccff4eb2f3c6a797c5f56807c0d
Howard Pritchard <howardp@lanl.gov>
2022-03-29 09:39:24 -0700

    use slurm.branch.out for sbatch output
    Signed-off-by: Howard Pritchard <howardp@lanl.gov>

--- run_mtt_scron.sh
+++ run_mtt_scron.sh
@@ -9,4 +9,4 @@ if [ $# -eq 0 ] ; then
 else
   BRANCH=$1
 fi
-sbatch -Am3169_g -t 2:00:00 -N 4 -C gpu  --gpus-per-node=1 ./run_mtt.sh $BRANCH
+sbatch -Am3169_g -o slurm.$BRANCH.out -J mtt-$BRANCH -t 2:00:00 -N 4 -C gpu  --gpus-per-node=1 ./run_mtt.sh $BRANCH
