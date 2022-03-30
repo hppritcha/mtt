daa3d59d0ae61363ed9c6c0a86fe4b5e3c9d66f4
Howard Pritchard <howardp@lanl.gov>
2022-03-30 11:20:19 -0400

    turn back on reporter in run_mtt
    Signed-off-by: Howard Pritchard <howardp@lanl.gov>

--- run_mtt.sh
+++ run_mtt.sh
@@ -6,6 +6,8 @@ module unload cray-libsci
 module unload cray-mpich
 module unload cray-dsmml
 
+env
+sleep 60
 cd $HOME/mtt
 if [ $# -eq 0 ] ; then
   #
@@ -32,5 +34,5 @@ else
         echo "Something went wrong with batch job"
     fi
 fi
-#pyclient/pymtt.py --verbose  iu_reporter_$BRANCH.ini
+pyclient/pymtt.py --verbose  iu_reporter_$BRANCH.ini
 
