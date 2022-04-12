3495179522ccd2d62544be4ddb88fe489581bc18
Howard Pritchard <howardp@lanl.gov>
2022-04-12 10:41:10 -0400

    add already built ompi ini and sh file
    Signed-off-by: Howard Pritchard <howardp@lanl.gov>

---
+++ test_my_ompi.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -l
+
+#module load python/3.7-anaconda-2019.07
+module load PrgEnv-gnu
+module unload cray-libsci
+module unload cray-mpich
+module unload cray-dsmml
+
+export PATH=/ccs/home/howardp/ompi/install/bin:$PATH
+#export OMPI_MCA_pml=ob1
+export PRTE_MCA_plm=ssh
+export OMPI_MCA_btl=self,sm,ofi
+export PRTE_MCA_plm_ssh_pass_libpath=/opt/cray/pe/gcc/11.2.0/snos/lib/../lib64
+
+cd $HOME/mtt
+. $HOME/prep_libfab.sh
+echo "=========================MODULESS================"
+module list
+echo "=========================USING THIS MPICC================"
+which mpicc
+rm -f -r test_scratch/*
+export MTT_HOME=$PWD
+pyclient/pymtt.py --verbose  test_my_ompi.ini
+
