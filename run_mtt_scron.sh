7dde73da07b017c3f47d43007e16b69c8475f189
Howard Pritchard <howardp@lanl.gov>
2022-06-23 10:49:17 -0700

    perlmutter: run on single node for now
    Signed-off-by: Howard Pritchard <howardp@lanl.gov>

--- run_mtt_scron.sh
+++ run_mtt_scron.sh
@@ -10,3 +10,4 @@ else
   BRANCH=$1
 fi
 sbatch -Am3169_g -o slurm.$BRANCH.out -J mtt-$BRANCH -t 2:00:00 -N 4 -C gpu  --gpus-per-node=1 ./run_mtt.sh $BRANCH
+#sbatch -Am3169_g -J mtt-$BRANCH -t 2:00:00 -N 1 -C gpu  --gpus-per-node=1 ./run_mtt.sh $BRANCH
