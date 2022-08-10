3cfe1a11994b1ab1d1bc0619ed002d62174a1eaa
Howard Pritchard <howardp@lanl.gov>
2022-08-10 17:34:13 -0400

    updates to the test_my_ompi script
    Signed-off-by: Howard Pritchard <howardp@lanl.gov>

--- test_my_ompi.sh
+++ test_my_ompi.sh
@@ -7,13 +7,16 @@ module unload cray-mpich
 module unload cray-dsmml
 
 export PATH=/ccs/home/howardp/ompi/install/bin:$PATH
-export OMPI_MCA_pml=ob1
+#export OMPI_MCA_pml=cm
+export FI_PROVIDER=cxi
 export PRTE_MCA_plm=ssh
 export OMPI_MCA_btl=self,sm,ofi
 export PRTE_MCA_plm_ssh_pass_libpath=/opt/cray/pe/gcc/11.2.0/snos/lib/../lib64
+export PRTE_MCA_prte_if_include=hsn0
+export OMPI_MCA_smsc=cma
 
 cd $HOME/mtt
-. $HOME/prep_libfab.sh
+#. $HOME/prep_libfab.sh
 echo "=========================MODULESS================"
 module list
 echo "=========================USING THIS MPICC================"
@@ -21,4 +24,6 @@ which mpicc
 rm -f -r test_scratch/*
 export MTT_HOME=$PWD
 pyclient/pymtt.py --verbose  test_my_ompi.ini
+grep FAIL test_scratch/mttresults.txt
+grep OUT test_scratch/mttresults.txt
 
