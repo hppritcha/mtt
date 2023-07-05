650ec4d7a2422170d03a336745eb3b815b1a5c38
Howard Pritchard <howardp@lanl.gov>
2023-07-05 12:02:56 -0400

    crusher: script updates
    Signed-off-by: Howard Pritchard <howardp@lanl.gov>

--- run_mtt.sh
+++ run_mtt.sh
@@ -7,6 +7,10 @@ module unload cray-mpich
 module unload cray-dsmml
 module load rocm
 
+export PRTE_MCA_ras_slurm_use_entire_allocation=1
+export PRTE_MCA_ras_base_launch_orted_on_hn=1
+module load libtool
+
 env
 sleep 60
 cd $HOME/mtt
@@ -25,10 +29,12 @@ rm -f -r $SCRATCH_DIR
 export MTT_HOME=$PWD
 echo "============== Testing $BRANCH  ==============="
 pyclient/pymtt.py --verbose  get_ompi_$BRANCH.ini
+echo $?
 if [ $? -ne 0 ]
 then
     echo "Something went wrong with fetch/build phase"
 else
+    rm slurm.$BRANCH.out
     echo "============== Submitting batch job for Testing $BRANCH  ==============="
     jobid=`sbatch -o slurm.$BRANCH.out --wait --parsable -N 4  -N 4 -AGEN010_crusher -t 8:00:00 --tasks-per-node=32 -J $BRANCH ./run_mtt_backend.sh $BRANCH`
     if [ $jobid -eq 1 ]; then
--- run_mtt_backend.sh
+++ run_mtt_backend.sh
@@ -5,6 +5,17 @@
 # somethings borked with Intel at the moment
 #
 module load PrgEnv-gnu
+module unload cray-libsci
+module unload cray-mpich
+module unload cray-dsmml
+module load rocm
+
+export PRTE_MCA_ras_slurm_use_entire_allocation=1
+export PRTE_MCA_ras_base_launch_orted_on_hn=1
+export PRTE_MCA_plm=slurm
+
+module list
+
 if [ $# -eq 0 ] ; then
   BRANCH=master
 else
@@ -12,5 +23,8 @@ else
 fi
 cd $HOME/mtt
 export MTT_HOME=$PWD
+echo "##########################################"
+which mpirun
+echo "##########################################"
 pyclient/pymtt.py --verbose run_ibm_tests_mpirun_$BRANCH.ini
 
