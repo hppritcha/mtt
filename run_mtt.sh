e5368fc7299feb94edddb9fad1bccacfe97689e4
Howard Pritchard <howardp@lanl.gov>
2024-04-29 15:29:22 -0400

    don't use NSF tmp filesystem
    Signed-off-by: Howard Pritchard <howardp@lanl.gov>

--- run_mtt.sh
+++ run_mtt.sh
@@ -1,10 +1,16 @@
 #!/bin/bash -l
 
-module load python-3.7.7-gcc-4.8.5-hw4wxvz           
-module load gcc-9.1.0-gcc-4.8.5-nhd4fe4     
-module load py-pyyaml-5.3.1-gcc-4.8.5-vl67ise
-module load py-setuptools-46.1.3-gcc-4.8.5-styz44m   
-module load py-pip-19.3-gcc-4.8.5-zifcadu
+# these not needed after upgrade to RHEL 8
+#module load python-3.7.7-gcc-4.8.5-hw4wxvz           
+#module load gcc-9.1.0-gcc-4.8.5-nhd4fe4     
+#module load py-pyyaml-5.3.1-gcc-4.8.5-vl67ise
+#module load py-setuptools-46.1.3-gcc-4.8.5-styz44m   
+#module load py-pip-19.3-gcc-4.8.5-zifcadu
+
+#
+# default /tmp on stretch is borked
+#
+#export TMPDIR=$HOME/mtt_tmpdir
 
 #
 # somethings borked with Intel at the moment
@@ -30,7 +36,7 @@ then
     echo "Something went wrong with fetch/build phase"
 else
 echo "============== Submitting batch job for Testing $BRANCH  ==============="
-jobid=`sbatch -o slurm.$BRANCH.out --wait --parsable -N 4 --time=6:00:00 --tasks-per-node=8 ./run_mtt_backend.sh $BRANCH`
+jobid=`sbatch -o slurm.$BRANCH.out --exclude=st35 --wait --parsable -N 4 --time=6:00:00 --tasks-per-node=8 ./run_mtt_backend.sh $BRANCH`
 if [ $jobid -eq 1 ]; then
     echo "Something went wrong with batch job"
 fi
