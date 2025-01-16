61f4b12720dd8887b17e42f0a946f50ab9398056
Howard Pritchard <howardp@lanl.gov>
2025-01-16 07:09:15 -0800

    perlmutter: add some tests to skip list
    and other updates
    
    Signed-off-by: Howard Pritchard <howardp@lanl.gov>

--- run_mtt_backend.sh
+++ run_mtt_backend.sh
@@ -2,15 +2,21 @@
 
 module load python
 module load PrgEnv-gnu
-#
-# somethings borked with Intel at the moment
-#
+module unload cray-libsci
+module unload cray-mpich
+module unload cray-dsmml
+module load cudatoolkit
+
+export PRTE_MCA_ras_slurm_use_entire_allocation=1
+export PRTE_MCA_ras_base_launch_orted_on_hn=1
+module load cudatoolkit
+
 if [ $# -eq 0 ] ; then
   BRANCH=master
 else
   BRANCH=$1
 fi
-cd $HOME/mtt_branch
+cd $HOME/mtt_perlmutter
 export MTT_HOME=$PWD
 pyclient/pymtt.py --verbose run_ibm_tests_mpirun_$BRANCH.ini
 
