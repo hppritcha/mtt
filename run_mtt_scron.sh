9a3c6bb456cd5f06f2e01b37146d7169727fb86c
Howard Pritchard <howardp@lanl.gov>
2026-02-03 06:56:40 -0800

    NERSC: start testing 6.0.x
    Signed-off-by: Howard Pritchard <howardp@lanl.gov>

--- run_mtt_scron.sh
+++ run_mtt_scron.sh
@@ -9,6 +9,7 @@ if [ $# -eq 0 ] ; then
 else
   BRANCH=$1
 fi
+./run_mtt.sh v6.0.x
 ./run_mtt.sh v5.0.x
 ./run_mtt.sh master
 #dep_id=`sbatch --parsable -Am3169 -o slurm.master.out -J mtt-master -N 1 -C cpu -t 4:00:00  -q regular ./run_mtt.sh master`
